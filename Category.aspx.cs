e9fadaa2c1568b5167512ee46be3a16d8b272a76
AjayBoreddy <96489284+AjayBoreddy@users.noreply.github.com>
2022-03-29 15:33:38 +0530

    Add files via upload

---
+++ Category.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace Foodie.Admin
+{
+    public partial class Category : System.Web.UI.Page
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter sda;
+        DataTable dt;
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnAddOrUpdate_Click(object sender, EventArgs e)
+        {
+            string actionName = string.Empty, imagePath = string.Empty, fileExtension = string.Empty;
+            bool isVaildToExecute = false;
+            int categoryId = Convert.ToInt32(hdnId.Value);
+            con = new SqlConnection(Connection.GetConnectionString());
+            cmd = new SqlCommand("category_crud", con);
+            cmd.Parameters.AddWithValue("@Action", categoryId == 0 ? "INSERT" : "UPDATE");
+            cmd.Parameters.AddWithValue("@CategoryId", categoryId);
+            cmd.Parameters.AddWithValue("@Name",txtName.Text.Trim());
+            cmd.Parameters.AddWithValue("IsActive", cbIsActive.Checked);
+            if (fuCategoryImage.HasFile)
+            {
+                if (Utils.IsValidExtension(fuCategoryImage.FileName))
+                {
+                    Guid obj = Guid.NewGuid();
+                    fileExtension = Path.GetExtension(fuCategoryImage.FileName);
+                    imagePath = "Images/Category/" + obj.ToString() + fileExtension;
+                    fuCategoryImage.PostedFile.SaveAs(Server.MapPath("/Images/Category") + obj.ToString() + fileExtension);
+                    cmd.Parameters.AddWithValue("@ImageUrl", imagePath);
+                    isVaildToExecute = true;
+
+                }
+                else
+                {
+                    lblMsg.Visible = true;
+                    lblMsg.Text = "Please select .jpg,.jpeg or .png image";
+                    lblMsg.CssClass = "alert alert-danger";
+                    isVaildToExecute = false;
+
+                }
+
+
+            }
+            else
+            {
+                isVaildToExecute = true;
+
+
+            }
+            if (isVaildToExecute)
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                try
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    actionName = categoryId == 0 ? "inserted" : "updated";
+                    lblMsg.Visible = true;
+                    lblMsg.Text = "Category" + actionName + "successfully!";
+                    lblMsg.CssClass = "alert alert-success";
+                    //getCategories();
+                    Clear();
+
+
+                }
+                catch (Exception ex)
+                {
+                    lblMsg.Visible = true;
+                    lblMsg.Text = "Error" + ex.Message;
+                    lblMsg.CssClass = "alert alert-danger";
+                    ;
+                }
+                finally
+                {
+                    con.Close();
+                }
+
+
+            }
+
+        }
+
+        private void Clear()
+        {
+            txtName.Text = string.Empty;
+            cbIsActive.Checked = false;
+            hdnId.Value = "0";
+            btnAddOrUpdate.Text = "Add";
+
+        }
+    }
+}
